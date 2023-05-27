a72c7a04807f72dbdded97d095f6669f7858d678
Dhananjay Dogne <111991487+DhananjayDogne@users.noreply.github.com>
2023-05-27 22:24:58 +0530

    Update routes.js

--- auth/routes.js
+++ auth/routes.js
@@ -1,1 +1,4 @@
-export const token_path = "auth/signin/"
+export const token_path_login = "auth/signin/"
+export const token_path_signup= "auth/signup/"
+export const token_path_details="auth/api/token/"
+export const token_path_create_project="auth/projects"
