886cebb63032e88d6bcadeadbbb1cd30634d5e44
Dhananjay Dogne <111991487+DhananjayDogne@users.noreply.github.com>
2023-05-27 22:26:06 +0530

    Update index.js of auth -> project

---
+++ auth/project/index.js
@@ -0,0 +1,9 @@
+import {secretCreateProject} from "./create_project.js";
+import { secretNewRequest } from "./new_request.js";
+import {secretNewRequestCopy} from "./new_request_copy.js";
+import {secretCreateProjectCopy} from "./create_project_copy.js";
+
+export const create_project=secretCreateProject;
+export const create_project_copy=secretCreateProjectCopy;
+export const new_request=secretNewRequest;
+export const new_request_copy=secretNewRequestCopy;
