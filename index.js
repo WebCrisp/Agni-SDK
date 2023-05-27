a328e8807fa3c06e71b14b2014395e078db2d789
Dhananjay Dogne <111991487+DhananjayDogne@users.noreply.github.com>
2023-05-27 22:41:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import { config } from 'dotenv'
 import axios from 'axios'
-import { login } from './auth/user/index.js';
+//user 
+import { login,signup,details } from './auth/user/index.js';
+
+//project 
+import {create_project,create_project_copy, new_request , new_request_copy} from './auth/project/index.js';
 
 config();
 
-console.log(await login({email:"root@gmail.com", password:"1234"}));
+// call for user
+// console.log(await login({email:"may@gmail.com", password:"12345"}));
+// console.log(await signup({username:"mayur",email:"mayur@gmail.com",first_name:"mayur",last_name:"mayur", password:"12345"}));
+// console.log(await details({email:"root@gmail.com",password:"1234"}));
+
+// call for project
+// console.log(await create_project({user_email:"root@gmail.com",project_name:"Haku021",description:"description of project"}));
+// console.log(await create_project_copy({user_email:"root@gmail.com",project_name:"Haku021",description:"description of project"}));
+// console.log(await new_request({project_id:"026e2860-c726-11ed-a0b4-8030493e60c6",api_key:"2bf30adb-00f2-432a-a31d-6a554444c7bf"}));
+// console.log(await new_request_copy({project_id:"026e2860-c726-11ed-a0b4-8030493e60c6",api_key:"2bf30adb-00f2-432a-a31d-6a554444c7bf",name:"Haku00",user:"root@gmail.com",description:"This is desc new"}));
