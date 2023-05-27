0c36881fcc548e911cf08593eabcd7afffd436e0
Dhananjay Dogne <111991487+DhananjayDogne@users.noreply.github.com>
2023-05-27 22:21:05 +0530

    Update index.js
    update index.js in user

--- auth/user/index.js
+++ auth/user/index.js
@@ -1,3 +1,7 @@
 import { secretLogin } from "./login.js";
+import { secretSignUp } from "./signup.js";
+import {secretDetails} from "./details.js";
 
 export const login = secretLogin;
+export const signup = secretSignUp;
+export const details = secretDetails;
