148da9209375c1b32562585ba5fbae5dbe69d882
Dhananjay Dogne <111991487+DhananjayDogne@users.noreply.github.com>
2023-06-07 13:58:57 +0530

    Update routes.js

--- database/routes.js
+++ database/routes.js
@@ -1,1 +1,5 @@
+export const token_path_query_document="auth/projects/";
+export const token_path_create_sub_collection="store/create-sub-collection/";
 export const token_path_create_project="store/add-document/";
+export const token_path_delete_document="store/delete-document/30b8bc31-e385-47ec-abae-a41b0cdf6968";
+export const token_path_update_document="store/update-document/";
