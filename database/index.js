63d722198afa59390d753aebf901063d08f59920
Dhananjay Dogne <111991487+DhananjayDogne@users.noreply.github.com>
2023-06-07 13:59:27 +0530

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,4 +1,11 @@
+import {secretQueryDocument} from "./query_document.js";
+import {secretSubCollection} from "./create_sub_collection.js";
 import { secretAddDocument } from "./add_document.js";
+import {secretDeleteDocument} from "./delete_document.js";
+import {secretUpdateDocument} from "./update_document.js";
 
+export const query_document =secretQueryDocument;
+export const create_sub_collection =secretSubCollection;
 export const add_document =secretAddDocument;
-
+export const delete_document =secretDeleteDocument;
+export const update_document =secretUpdateDocument;
